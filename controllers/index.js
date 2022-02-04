724db66c1e4420a0fded6e0324a927815e1a0044
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-02-03 20:48:31 -0600

    tried to build some initial routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,13 @@
+const router = require('express').Router();
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require()
+
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+router.get('*', function(req, res) {
+    res.render('login');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 86400,
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 86400,
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
